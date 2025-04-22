08c254cac06791dc728e2c54f89dbd50a6cc4286
AndrewSebastianH <andrew.hardianta@bankneo.co.id>
2025-04-23 00:55:36 +0700

    feat: implemented skeletonizer for calendarPage

--- thesis_frontend/lib/screens/main/calendar_page.dart
+++ thesis_frontend/lib/screens/main/calendar_page.dart
@@ -1,5 +1,6 @@
 // import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:table_calendar/table_calendar.dart';
 // import 'package:http/http.dart' as http;
 // import 'package:thesis_frontend/config/apiConfig.dart';
@@ -21,6 +22,7 @@ class _EmotionCalendarPageState extends State<EmotionCalendarPage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   List<EmotionLog> _selectedEvents = [];
+  bool _isLoading = true;
 
   @override
   void initState() {
@@ -54,6 +56,7 @@ class _EmotionCalendarPageState extends State<EmotionCalendarPage> {
     setState(() {
       emotionEvents = eventMap;
       _selectedEvents = _getEventsForDay(_selectedDay ?? _focusedDay);
+      _isLoading = false;
     });
   }
 
@@ -155,152 +158,152 @@ class _EmotionCalendarPageState extends State<EmotionCalendarPage> {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          TableCalendar<EmotionLog>(
-            firstDay: DateTime.utc(2024, 1, 1),
-            lastDay: DateTime.utc(2030, 12, 31),
-            focusedDay: _focusedDay,
-            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-            calendarFormat: CalendarFormat.month,
-            availableCalendarFormats: const {CalendarFormat.month: 'Month'},
-            eventLoader: _getEventsForDay,
-            onDaySelected: (selectedDay, focusedDay) {
-              if (!mounted) return;
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-                _selectedEvents = _getEventsForDay(selectedDay);
-              });
-            },
-            calendarStyle: const CalendarStyle(
-              selectedDecoration: BoxDecoration(
-                color: Color(0xFFFF7F50),
-                shape: BoxShape.circle,
-              ),
-              todayDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.fromBorderSide(
-                  BorderSide(color: Color(0xFFFF7F50), width: 2),
+      body: Skeletonizer(
+        enabled: _isLoading,
+        child: Column(
+          children: [
+            TableCalendar<EmotionLog>(
+              firstDay: DateTime.utc(2024, 1, 1),
+              lastDay: DateTime.utc(2030, 12, 31),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              calendarFormat: CalendarFormat.month,
+              availableCalendarFormats: const {CalendarFormat.month: 'Month'},
+              eventLoader: _getEventsForDay,
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!mounted) return;
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  _selectedEvents = _getEventsForDay(selectedDay);
+                });
+              },
+              calendarStyle: const CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                  color: Color(0xFFFF7F50),
+                  shape: BoxShape.circle,
+                ),
+                todayDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.fromBorderSide(
+                    BorderSide(color: Color(0xFFFF7F50), width: 2),
+                  ),
+                ),
+                todayTextStyle: TextStyle(
+                  color: Color(0xFFFF7F50),
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              todayTextStyle: TextStyle(
-                color: Color(0xFFFF7F50),
-                fontWeight: FontWeight.bold,
+              calendarBuilders: CalendarBuilders(
+                markerBuilder: (context, date, events) {
+                  if (events.isEmpty) return const SizedBox();
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:
+                        events.map((e) {
+                          final color =
+                              e.userId == currentUser?.id
+                                  ? Colors.orange
+                                  : Colors.blue;
+                          return Container(
+                            width: 6,
+                            height: 6,
+                            margin: const EdgeInsets.symmetric(horizontal: 0.5),
+                            decoration: BoxDecoration(
+                              color: color,
+                              shape: BoxShape.circle,
+                            ),
+                          );
+                        }).toList(),
+                  );
+                },
               ),
             ),
-
-            calendarBuilders: CalendarBuilders(
-              markerBuilder: (context, date, events) {
-                if (events.isEmpty) return const SizedBox();
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:
-                      events.map((e) {
-                        final color =
-                            (e).userId == currentUser?.id
-                                ? Colors.orange
-                                : Colors.blue;
-                        return Container(
-                          width: 6,
-                          height: 6,
-                          margin: const EdgeInsets.symmetric(horizontal: 0.5),
-                          decoration: BoxDecoration(
-                            color: color,
-                            shape: BoxShape.circle,
-                          ),
-                        );
-                      }).toList(),
-                );
-              },
-            ),
-          ),
-
-          const SizedBox(height: 12),
-          Expanded(
-            child:
-                _selectedEvents.isEmpty
-                    ? const Center(child: Text("No emotion logs for this day."))
-                    : ListView.builder(
-                      itemCount: _selectedEvents.length,
-                      itemBuilder: (context, index) {
-                        final log = _selectedEvents[index];
-
-                        final isCurrentUser = log.userId == currentUser?.id;
-
-                        final username =
-                            isCurrentUser
-                                ? "You"
-                                : relatedUser?.username ?? "Someone";
-
-                        final avatarAsset =
-                            isCurrentUser
-                                ? userProvider.userAvatarAsset
-                                : userProvider.relatedUserAvatarAsset;
-
-                        return Card(
-                          color:
+            const SizedBox(height: 12),
+            Expanded(
+              child:
+                  _selectedEvents.isEmpty
+                      ? const Center(
+                        child: Text("No emotion logs for this day."),
+                      )
+                      : ListView.builder(
+                        itemCount: _selectedEvents.length,
+                        itemBuilder: (context, index) {
+                          final log = _selectedEvents[index];
+                          final isCurrentUser = log.userId == currentUser?.id;
+                          final username =
                               isCurrentUser
-                                  ? Colors.orange[100]
-                                  : Colors.blue[100],
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          elevation: 2,
-                          margin: const EdgeInsets.symmetric(
-                            vertical: 5,
-                            horizontal: 16,
-                          ),
-                          child: ListTile(
-                            onTap:
-                                () => _showLogDetailsDialog(
-                                  context,
-                                  log,
-                                  username,
-                                  avatarAsset,
-                                  isCurrentUser,
-                                ),
-                            contentPadding: const EdgeInsets.symmetric(
+                                  ? "You"
+                                  : relatedUser?.username ?? "Someone";
+                          final avatarAsset =
+                              isCurrentUser
+                                  ? userProvider.userAvatarAsset
+                                  : userProvider.relatedUserAvatarAsset;
+
+                          return Card(
+                            color:
+                                isCurrentUser
+                                    ? Colors.orange[100]
+                                    : Colors.blue[100],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            elevation: 2,
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 5,
                               horizontal: 16,
-                              vertical: 8,
                             ),
-                            leading: CircleAvatar(
-                              radius: 20,
-                              backgroundImage: AssetImage(avatarAsset),
-                            ),
-                            title: Text(
-                              "${log.emotion[0].toUpperCase()}${log.emotion.substring(1)}",
-                              style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
+                            child: ListTile(
+                              onTap:
+                                  () => _showLogDetailsDialog(
+                                    context,
+                                    log,
+                                    username,
+                                    avatarAsset,
+                                    isCurrentUser,
+                                  ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 8,
+                              ),
+                              leading: CircleAvatar(
+                                radius: 20,
+                                backgroundImage: AssetImage(avatarAsset),
+                              ),
+                              title: Text(
+                                "${log.emotion[0].toUpperCase()}${log.emotion.substring(1)}",
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    DateFormat.yMMMMd().format(log.date),
+                                    style: const TextStyle(fontSize: 12),
+                                  ),
+                                  const SizedBox(height: 2),
+                                  Text(
+                                    isCurrentUser
+                                        ? "You shared this feeling"
+                                        : "$username shared this feeling",
+                                    style: TextStyle(
+                                      fontSize: 11,
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  DateFormat.yMMMMd().format(log.date),
-                                  style: const TextStyle(fontSize: 12),
-                                ),
-                                const SizedBox(height: 2),
-                                Text(
-                                  isCurrentUser
-                                      ? "You shared this feeling"
-                                      : "$username shared this feeling",
-                                  style: TextStyle(
-                                    fontSize: 11,
-                                    fontStyle: FontStyle.italic,
-                                    color: Colors.grey[700],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-          ),
-        ],
+                          );
+                        },
+                      ),
+            ),
+          ],
+        ),
       ),
     );
   }
